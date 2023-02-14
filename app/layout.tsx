f69cb8d7dcf479c9c9ea5abe1239db1faf3e21d8
Zaid Mukaddam <zaidaltafmukaddam@gmail.com>
2023-02-14 18:26:08 +0530

    Remove next-auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,4 @@
-import { getServerSession } from "next-auth/next";
-
 import "../styles/globals.css";
-
-import { SessionProvider } from "../lib/sessionProvider";
 import { Navbar } from "./Navbar";
 import { Footer } from "../components/Footer";
 import { Analytics } from "./Analytics";
@@ -12,13 +8,11 @@ export default async function RootLayout({
 }: {
 	children: React.ReactNode;
 }) {
-	const session = await getServerSession();
 
 	return (
 		<html lang="en">
 			<body className="h-fill scroll-smooth bg-black font-sans text-white">
-				<SessionProvider session={session}>
-					{/* <style jsx global>
+				{/* <style jsx global>
 					{`
 						#__next {
 							height: -webkit-fill-available;
@@ -26,13 +20,12 @@ export default async function RootLayout({
 						}
 					`}
 				</style> */}
-					<Navbar />
-					<div className="relative z-[1] mb-[58.25px] bg-black">
-						{children}
-					</div>
-					{process.env.NODE_ENV === "production" && <Analytics />}
-					<Footer />
-				</SessionProvider>
+				<Navbar />
+				<div className="relative z-[1] mb-[58.25px] bg-black">
+					{children}
+				</div>
+				{process.env.NODE_ENV === "production" && <Analytics />}
+				<Footer />
 			</body>
 		</html>
 	);
