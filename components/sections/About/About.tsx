cb2305308f6bf6a06e6cc6c0a966a0b30c301fbd
Zaid Mukaddam <zaidaltafmukaddam@gmail.com>
2023-02-16 13:14:40 +0530

    Fix unescaped entities

--- components/sections/About/About.tsx
+++ components/sections/About/About.tsx
@@ -50,8 +50,8 @@ export const AboutSection = ({
 						<p className="text-body xl:max-w-[680px]">
 						An apparently mundane auto rickshaw can take us places and be instrumental in experiences that will be latched onto our minds forever, and the simple activity of listening to music can open up our prospects for thrill and adventure...
 						</p>
-						<p className="text-body xl:max-w-[680px]">We're redefining normalcy, and we're thrilled to have you along on the ride…</p>
-						<p className="text-body xl:max-w-[680px]">This is Safarnama with Sattva 2023, and we're just getting started!</p>
+						<p className="text-body xl:max-w-[680px]">We&apos;re redefining normalcy, and we&apos;re thrilled to have you along on the ride…</p>
+						<p className="text-body xl:max-w-[680px]">This is Safarnama with Sattva 2023, and we&apos;re just getting started!</p>
 					</div>
 					{/* {!standAlone && <NowPlaying />} */}
 				</div>
